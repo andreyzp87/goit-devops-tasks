0f28103b55b9a8c5739c5ad2909d93256004e37e
Andrii Diachenko <andreyzp87@gmail.com>
2025-07-22 21:10:34 +0200

    implement tasks 8-9

---
+++ lesson-8-9/main.tf
@@ -0,0 +1,103 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "s3_backend" {
+  source      = "./modules/s3-backend"
+  bucket_name = "andreyzp87-state-goit-devops"
+  table_name  = "terraform-locks"
+}
+
+module "vpc" {
+  source             = "./modules/vpc"
+  vpc_cidr_block     = "10.0.0.0/16"
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  vpc_name           = "lesson-9-vpc"
+}
+
+module "ecr" {
+  source       = "./modules/ecr"
+  ecr_name     = "lesson-9-django-app"
+  scan_on_push = true
+}
+
+module "eks" {
+  source           = "./modules/eks"
+  cluster_name     = "lesson-9-eks-cluster"
+  cluster_version  = "1.28"
+  vpc_id           = module.vpc.vpc_id
+  subnet_ids       = module.vpc.public_subnet_ids
+  node_group_name  = "lesson-9-node-group"
+  instance_types   = ["t2.micro"]
+  desired_capacity = 1
+  max_capacity     = 2
+  min_capacity     = 1
+}
+
+data "aws_eks_cluster" "eks" {
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
+module "jenkins" {
+  source            = "./modules/jenkins"
+  kubeconfig        = data.aws_eks_cluster.eks.endpoint
+  cluster_name      = module.eks.cluster_name
+  oidc_provider_arn = module.eks.oidc_provider_arn
+  oidc_provider_url = module.eks.oidc_provider_url
+
+  depends_on = [module.eks]
+
+  providers = {
+    helm       = helm
+    kubernetes = kubernetes
+  }
+}
+
+module "argo_cd" {
+  source        = "./modules/argo_cd"
+  namespace     = "argocd"
+  chart_version = "5.46.4"
+
+  depends_on = [module.eks]
+}
+
---
+++ lesson-8-9/modules/vpc/vpc.tf
@@ -0,0 +1,67 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+# Public subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-public-subnet-${count.index + 1}"
+  }
+}
+
+# Private subnets
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.vpc_name}-private-subnet-${count.index + 1}"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+#TODO
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.vpc_name}-nat-eip"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.vpc_name}-nat"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  depends_on = [aws_internet_gateway.igw]
+}
